f632fd0df242e16c2e613697deb28b6f4dece822
zixi948 <140379153+zixi948@users.noreply.github.com>
2023-10-16 18:14:26 +1030

    Add files via upload

---
+++ Tower1.h
@@ -0,0 +1,27 @@
+#ifndef TOWER1_H
+#define TOWER1_H
+#include <cstdlib> // rand
+#include "Tower.h"
+
+class Tower1 : public Tower {
+    private:
+        bool splash;
+        bool x1_5;
+
+    public:
+        Tower1(int id, int x, int y, int health, int damage, int attackRange, float attackSpeed):
+        Tower(id, x, y, health, damage, attackRange, attackSpeed) {
+            splash = !(rand() % 5);
+            x1_5 = splash ? !(rand() % 5) : false;
+        }
+
+        bool isSplash() {
+            return splash;
+        }
+
+        bool isX1_5() {
+            return x1_5;
+        }
+
+}
+#endif
